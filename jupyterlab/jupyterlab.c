b42a49bfcab303350f1629fc237b75b72eb9cc8b
fredjt <61065187+fredjt@users.noreply.github.com>
2021-07-12 21:55:58 -0400

    Add command line argument support
    Add support for command line arguments for opening a notebook file when launching.

--- jupyterlab/jupyterlab.c
+++ jupyterlab/jupyterlab.c
@@ -1,6 +1,9 @@
+#include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-	system("jupyter lab");
+int main(int argc, char *argv[]) {
+	char com[100];
+	sprintf(com, "jupyter lab \"%s\"", argc > 1 ? argv[1] : "");
+	system(com);
 	return (0);
 }
